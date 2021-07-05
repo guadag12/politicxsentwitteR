1d96f8cf0f41649203f3514358a8a0fc080ec5e8
guadag12 <guadag12@outlook.com>
2021-07-05 11:08:43 -0300

    changes

--- R/get_friends_followers_all.R
+++ R/get_friends_followers_all.R
@@ -26,7 +26,7 @@
 #' @seealso get_friends_followers
 
 
-get_friends_followers_all <- function(category="all", country="ar", historic = F){
+get_friends_followers_all <- function(category="all", historic = T){
 
   url_path <- download_url(1)
   data_politicxs <- download_list()
@@ -35,7 +35,7 @@ get_friends_followers_all <- function(category="all", country="ar", historic = F
 
   #si es all & arg
 
-  if(category == "all" & country == "ar" & historic == F ){
+  if(category == "all" & historic == F ){
 
     #si no es historico
     if(!category %in% data_politicxs$category ){
@@ -51,7 +51,7 @@ get_friends_followers_all <- function(category="all", country="ar", historic = F
   }
 
   #si es historico
-  if(category == "all" & country == "ar" & historic == T ){
+  if(category == "all"  & historic == T ){
     if(!category %in% data_politicxs$category ){
       stop(paste0("the selected category doesn't exist. Trying choose between some of this:
                                                    'all','deputies','national executive','others','province servants', 'senators'. "))
@@ -64,7 +64,7 @@ get_friends_followers_all <- function(category="all", country="ar", historic = F
     data <- data_crec_db$find()}
   }
 
-  if((category != "all" | country != "ar" ) & historic == F){
+  if(category != "all"  & historic == F){
     if(!category %in% data_politicxs$category ){
       stop(paste0("the selected category doesn't exist. Trying choose between some of this:
                                                    'all','deputies','national executive','others','province servants', 'senators'. "))
@@ -80,7 +80,7 @@ get_friends_followers_all <- function(category="all", country="ar", historic = F
 
   }
 
-  if((category != "all" | country != "ar" ) & historic == T){
+  if(category != "all" & historic == T){
     if(!category %in% data_politicxs$category ){
       stop(paste0("the selected category doesn't exist. Trying choose between some of this:
                                                    'all','deputies','national executive','others','province servants', 'senators'. "))
@@ -92,10 +92,11 @@ get_friends_followers_all <- function(category="all", country="ar", historic = F
                                      url = url_path,
                                      verbose = TRUE)
     data <- data_crec_db$find()
-    data <- data[(data$screen_name %in% data_politicxs[(data_politicxs$category == category), "screen_name"]), ] }
+    data <- data[(data$screen_name %in% data_politicxs[(data_politicxs$category == category),
+                                                       "screen_name"]), ] }
 }
-  data <- data[, c("date", "screen_name", "followers_count", "friends_count", "listed_count", "statuses_count",
-                   "favourites_count")]
+  data <- data[,  c("screen_name","date", "followers_count", "friends_count", "listed_count", "statuses_count",
+                    "favourites_count")]
   return(data)
   cat(crayon::green$bold("Congrats, the data of", category, "is download"))
 }
