c28d8ec93e40d897cf43798d054e005ef5045ed9
Guadalupe Gonzalez <31290160+guadag12@users.noreply.github.com>
2021-07-08 11:43:58 -0300

    Delete R directory

--- R/politicxsentwitter.R
+++
@@ -1,7 +0,0 @@
-#' politicxsentwitteR: Data of Politicians of Argentina on Twitter
-#'
-#' Back up and reusable data of what the principal public servants talk on Twitter
-#'
-#' @docType package
-#' @name politicxsentwitteR
-"_PACKAGE"
